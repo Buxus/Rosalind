4d08ba2fb11523f1b9eee6d2a3733fc4f4ec14e2
Matt Wolf <mattwollf@gmail.com>
2018-03-26 23:55:10 -0400

    wut r 4loop

--- Introduction_to_Random_Strings/main.py
+++ Introduction_to_Random_Strings/main.py
@@ -6,7 +6,7 @@ import math
 def main():
     dna_file = open("./rosalind_prob.txt", 'r')
     dna_string = dna_file.readline().strip()
-    
+
     a_array = [num for num in dna_file.readline().split()]
 
     print(dna_string)
@@ -14,12 +14,12 @@ def main():
 
     AT = dna_string.count('A') + dna_string.count('T')
     CG = dna_string.count('C') + dna_string.count('G')
-    
-    b_array = [round(math.log10(((float(a_array[x])/2)**CG)*((.5-(float(a_array[x])/2))**AT)), 3) for x in range(len(a_array))]
+
+    b_array = [round(math.log10(((float(x)/2)**CG)*((.5-(float(x)/2))**AT)), 3) for x in a_array]
 
     for x in range(len(b_array)):
         print(b_array[x], end = " ")
     print("")
-    
+
 if __name__ == "__main__":
     main()
